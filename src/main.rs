05b0dca67eabfae8a0bdb76b6fce81363c8b317c
Daniel Boll <danielboll.academico@gmail.com>
2023-05-17 19:13:37 -0300

    refactor(main.rs): change protocol type from TCP to UDP in file transfer feat(main.rs): add user confirmation to stop the program

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   init_library_logger(log::LevelFilter::Debug);
 
   // Create two arc users
-  let alice = Arc::new(User::new(SocketAddr::new(
-    IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-    8000,
-  )));
-  let bob = Arc::new(User::new(SocketAddr::new(
-    IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-    8001,
-  )));
+  let alice = Arc::new(
+    User::new(SocketAddr::new(
+      IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+      8000,
+    ))
+    .await,
+  );
+  let bob = Arc::new(
+    User::new(SocketAddr::new(
+      IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+      8001,
+    ))
+    .await,
+  );
 
   // Start both user listening for incoming connections
   if let Err(e) = alice.clone().start_listening() {
@@ -29,10 +35,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   }
 
   alice
-    .create_file_transfer(bob.clone(), ProtocolType::Tcp)
+    .create_file_transfer(bob.clone(), ProtocolType::Udp)
     .await?
     .send("Cargo.toml")
     .await?;
+
+  // wait for user confirmation to stop
+  let mut input = String::new();
+  std::io::stdin().read_line(&mut input)?;
 
   Ok(())
 }
